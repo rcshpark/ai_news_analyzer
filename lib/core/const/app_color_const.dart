deb0bcd3ee11aca205b7fc3c6088fcd6d2ebe380
rcshpark <rcshpark@gmail.com>
2026-03-10 21:21:22 +0900

    feat: add additional color constants for UI elements

--- lib/core/const/app_color_const.dart
+++ lib/core/const/app_color_const.dart
@@ -5,13 +5,22 @@ class AppColors {
   static const Color darkBackground2 = Color(0xFF18181B);
   static const Color darkBackground2_60 = Color(0x9918181B);
   static const Color borderColor = Color(0xFF27272A);
+  static const Color panelBackground = Color(0xCC101117);
+  static const Color panelBorder = Color(0xFF2C3240);
+  static const Color panelShadow = Color(0x66000000);
   static const Color white = Colors.white;
   static const Color greyText1 = Color(0xFF71717B);
   static const Color greyText2 = Color(0xFF9F9FA9);
+  static const Color greyText3 = Color(0xFFCACFDB);
   static const Color accentBlue = Color(0xFF51A2FF);
   static const Color buttonBlue = Color(0xFF155DFC);
   static const Color loginButtonBackground = Color(0xFF004FFF);
   static const Color blueShadow = Color.fromRGBO(43, 127, 255, 0.5);
+  static const Color glowBlue = Color(0x663B82F6);
+  static const Color glowBlueSoft = Color(0x334EA1FF);
+  static const Color glowCyan = Color(0x3338BDF8);
+  static const Color iconBackground = Color(0xFF151821);
+  static const Color divider = Color(0xFF303645);
   static const Color guestButtonText = Color(0xFFD4D4D8);
   static const Color black = Colors.black;
 }
